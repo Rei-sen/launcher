083a588ee58ba7787a6ba92cb3c4ad9be563c26b
rei-sen <Qasde@Protonmail.ch>
2021-12-19 16:02:59 +0100

    Zakładka z nowościami

--- src/UI/NewsTab.cc
+++ src/UI/NewsTab.cc
@@ -1,4 +1,13 @@
 
 #include "UI/NewsTab.hh"
 
-NewsTab::NewsTab(State &s) : Tab("News", s) { end(); }
+#include <FL/Fl_Text_Display.H>
+#include <FL/Fl_Tree.H>
+
+NewsTab::NewsTab(State &s) : Tab("News", s) {
+  { new Fl_Tree(10, 35, 221, 410); }          // Fl_Tree* o
+  { new Fl_Text_Display(241, 35, 374, 30); }  // Fl_Text_Display* o
+  { new Fl_Text_Display(241, 75, 374, 370); } // Fl_Text_Display* o
+
+  end();
+}
