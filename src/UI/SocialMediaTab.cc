1a5cd3b40de97f58407b22e391e994d70982db17
rei-sen <Qasde@Protonmail.ch>
2021-12-19 16:15:05 +0100

    zakładka z mediami społecznościowymi

--- src/UI/SocialMediaTab.cc
+++ src/UI/SocialMediaTab.cc
@@ -1,4 +1,14 @@
 
 #include "UI/SocialMediaTab.hh"
 
-SocialMediaTab::SocialMediaTab(State &s) : Tab("Social media", s) { end(); }
+#include <FL/Fl_Table.H>
+
+SocialMediaTab::SocialMediaTab(State &s) : Tab("Social media", s) {
+  {
+    Fl_Table *o = new Fl_Table(10, 35, 605, 410);
+    o->cols(2);
+    o->end();
+  }
+
+  end();
+}
