148c32f6349c28ed59344cdfe1a0234f017542cd
rei-sen <Qasde@Protonmail.ch>
2021-12-19 16:01:41 +0100

    zakładka z ustawieniami

--- src/UI/SettingsTab.cc
+++ src/UI/SettingsTab.cc
@@ -1,4 +1,13 @@
 
 #include "UI/SettingsTab.hh"
 
-SettingsTab::SettingsTab(State &s) : Tab("Settings", s) { end(); }
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+
+SettingsTab::SettingsTab(State &s) : Tab("Settings", s) {
+  { new Fl_Input(250, 175, 135, 30, "Username"); } // Fl_Input* o
+  { new Fl_Button(280, 255, 72, 25, "Update"); }   // Fl_Button* o
+  { new Fl_Input(250, 215, 135, 30, "Password"); } // Fl_Input* o
+
+  end();
+}
