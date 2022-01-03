75e9273032a637b1c5930731161b55777392a586
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-03 21:08:15 +0100

    dodanie frontendu

--- include/UI/GameLibTab.hh
+++ include/UI/GameLibTab.hh
@@ -3,6 +3,12 @@
 
 #include "UI/Tab.hh"
 
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Browser.H>
+#include <FL/Fl_Text_Display.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Table.H>
+
 class GameLibTab : public Tab {
 public:
   GameLibTab(const GameLibTab &) = delete;
@@ -10,4 +16,46 @@ public:
   virtual ~GameLibTab() = default;
 
   GameLibTab(State &s);
+  void searchButtonMet();
+
+  void playButtonMet();
+  void updateDlcAndNameMet();
+
+  static void playButtonCallback(Fl_Widget *, void *v) {
+    ((GameLibTab *)v)->playButtonMet();
+  }
+
+    void instalButtonMet();
+
+  static void instalButtonCallback(Fl_Widget *, void *v) {
+      ((GameLibTab *)v)->instalButtonMet();
+  }
+
+    void uninstallButtonMet();
+
+  static void uninstalButtonCallback(Fl_Widget *, void *v) {
+      ((GameLibTab *)v)->uninstallButtonMet();
+  }
+
+    static void searchButtonCallback(Fl_Widget *, void *v) {
+    ((GameLibTab *)v)->searchButtonMet();
+  }
+
+      static void updateDlcAndNameCallback(Fl_Widget *, void *v) {
+    ((GameLibTab *)v)->updateDlcAndNameMet();
+  }
+
+private:
+  Fl_Input *searchInput;
+  Fl_Browser *gameList;
+// chyba w układzie trzeba coś zmienić
+//  Fl_Browser *dlcList;
+  Fl_Output *gameName;
+
+  Fl_Button *playButton;
+  Fl_Button *uninnstallButton;
+  Fl_Button *innstallButton;
+  Fl_Table *dlcTab;
+
+  Fl_Button *searchButton;
 };
