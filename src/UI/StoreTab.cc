343eae1783a94454edd4d0a9f131885ef537fe24
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-03 22:41:26 +0100

    dodanie frontendu StoreTab

--- src/UI/StoreTab.cc
+++ src/UI/StoreTab.cc
@@ -3,43 +3,45 @@
 
 #include <FL/Fl_Browser.H>
 #include <FL/Fl_Button.H>
+#include <FL/Fl_message.H>
 
-
-
-void StoreTab::searchButtonMet() {
-    // dodać wyszukanie gier w bazie danych
-  printf("wyszukiwanie");
-  gameName->value(searchInput->value());
-    
+void StoreTab::onSearchButton(Fl_Widget *, void *v) {
+    // poniżej sprawdzenie czy działa - wyświetlenie komunikatu + przepisanie wartości z search do name
+    printf("wyszukiwanie");
+    ((StoreTab *)v)->gameName->value(((StoreTab *)v)->searchInput->value());
 }
 
-void StoreTab::buyButtonMet() {
-    // dodać zakup gry/dlc
-  printf("zakup");
+void StoreTab::onBuyButton(Fl_Widget *, void *v) {
+  // poniżej sprawdzenie czy działa
+  fl_message("Game bought");
 }
 
+void StoreTab::onBrowserClick(Fl_Widget *, void *v) {
+  // poniżej sprawdzenie czy działa
+    printf("aktualizacja dlc");
+  // dodać sprawdzenie czy użytkownik posiada produkcje jeżeli nie deaktywować przycisk
+    ((StoreTab *)v)->buyButton->activate();
+ 
+// potem poprawne przypisania dać z connecta
+    ((StoreTab *)v)->priceLabel->value(((StoreTab *)v)->searchInput->value());
+    ((StoreTab *)v)->statusLabel->value(((StoreTab *)v)->searchInput->value());
 
-void StoreTab::updateDlcAndNameMet() {
-    // dodać załadowanie opisu do pola description oraz załadowanie listy dlc dla wybranej gry, dodatkowo ustawić nazwę gry na wybrane pole z listy produktów
-  printf("aktualizacja dlc");
-//    buyButton->activate();
-  buyButton->activate();
 
 }
 
-void StoreTab::updateNameFiedlMet() {
+void StoreTab::onDlcBrowserClick(Fl_Widget *, void *v) {
   // dodać załadowanie opisu do pola description oraz załadowanie listy dlc dla
   // wybranej gry, dodatkowo ustawić nazwę gry na wybrane pole z listy
   // produktów
   printf("nazwa gry = nazwa dlc");
-  buyButton->activate();
+  ((StoreTab *)v)->buyButton->activate();
 }
 
 
 StoreTab::StoreTab(State &s) : Tab("Store", s) {
         // dodać załadowanie listy gier oraz zaznaczenie którą grę się posiada a którą nie, 
         gameList = new Fl_Browser(10, 75, 221, 370); 
-        gameList->callback(updateDlcAndNameCallback, this);
+        gameList->callback(onBrowserClick, this);
    // Fl_Browser* o
 
   
@@ -49,7 +51,7 @@ StoreTab::StoreTab(State &s) : Tab("Store", s) {
   
     searchButton =
         new Fl_Button(159, 35, 72, 30, "search"); // Fl_Button* o
-    searchButton->callback(searchButtonCallback, this);
+    searchButton->callback(onSearchButton, this);
   
   
     Fl_Group *o = new Fl_Group(241, 35, 374, 410);
@@ -58,7 +60,7 @@ StoreTab::StoreTab(State &s) : Tab("Store", s) {
     
       buyButton = new Fl_Button(533, 85, 72, 30, "Buy");
       // dodać callbacl
-      buyButton->callback(buyButtonCallback, this);
+      buyButton->callback(onBuyButton, this);
       buyButton->deactivate();
      // Fl_Button* o
     
@@ -71,7 +73,7 @@ StoreTab::StoreTab(State &s) : Tab("Store", s) {
     
       dlcList = new Fl_Browser(251, 335, 354, 100, "DLC");
       dlcList->align(Fl_Align(FL_ALIGN_TOP));
-      dlcList->callback(updateNameFiedlCallback, this);
+      dlcList->callback(onDlcBrowserClick, this);
      // Fl_Browser* o
     
       description = new Fl_Text_Display(251, 125, 354, 178);
