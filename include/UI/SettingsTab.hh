f78d316510cbdfc304900a5bc828b0313c84c5ba
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-03 21:07:44 +0100

    dodanie frontendu

--- include/UI/SettingsTab.hh
+++ include/UI/SettingsTab.hh
@@ -2,6 +2,9 @@
 #pragma once
 
 #include "UI/Tab.hh"
+#include <FL/Fl.H>
+#include <string>
+#include <FL/Fl_Input.H>
 
 class SettingsTab : public Tab {
 public:
@@ -10,4 +13,15 @@ public:
   virtual ~SettingsTab() = default;
 
   SettingsTab(State &s);
+
+
+  void updateButtonMet();
+
+  static void updateButtonCallback(Fl_Widget *, void *v) {
+    ((SettingsTab *)v)->updateButtonMet();
+  }
+
+private:
+  Fl_Input *newNameField;
+  Fl_Input *newPasswordField;
 };
