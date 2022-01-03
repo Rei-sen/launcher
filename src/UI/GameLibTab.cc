85a58e6e677f2f2ccf470df1e2dba38d13574b49
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-03 21:06:23 +0100

    dodanie frontendu

--- src/UI/GameLibTab.cc
+++ src/UI/GameLibTab.cc
@@ -8,36 +8,80 @@
 #include <FL/Fl_Output.H>
 #include <FL/Fl_Table.H>
 
+
+void GameLibTab::updateDlcAndNameMet() {
+  // dodać załadowanie opisu do pola description oraz załadowanie listy dlc dla
+  // wybranej gry, dodatkowo ustawić nazwę gry na wybrane pole z listy
+  // produktów
+  printf("wybor");
+
+  // potem dać sprawdzenie stanu gry - czy jest zainstalowana i odpowiednio aktywować przyciski
+
+  playButton->activate();
+  uninnstallButton->activate();
+  innstallButton->activate();
+  gameName->value(searchInput->value());
+}
+
+
+void GameLibTab::searchButtonMet() {
+  // dodać wyszukanie gier i dlc w bazie danych 
+  printf("search");
+  gameName->value(searchInput->value());
+}
+
+void GameLibTab::playButtonMet() {
+// dodać komunikat imitujący poprawne uruchomienie gry
+  printf("play");
+
+}
+
+
+void GameLibTab::instalButtonMet() {
+  // dodać komunikat o instalowaniu gry, uaktywnić przycisk do gry i odinstalowania
+  printf("instal");
+}
+
+
+void GameLibTab::uninstallButtonMet() {
+  // dodać komunikat o odinstalowaniu gry, deaktywować przycisk do gry
+  printf("uninstall");
+//  buyButton->activate();
+}
+
+
 GameLibTab::GameLibTab(State &s) : Tab("Game library", s) {
 
-  { new Fl_Browser(10, 75, 221, 370); } // Fl_Browser* o
-  {
-    Fl_Input *o = new Fl_Input(10, 35, 139, 30);
-    o->align(Fl_Align(FL_ALIGN_CENTER));
-  }                                             // Fl_Input* o
-  { new Fl_Button(159, 35, 72, 30, "search"); } // Fl_Button* o
-  {
+    gameList = new Fl_Browser(10, 75, 221, 370); 
+    gameList->callback(updateDlcAndNameCallback, this);
+
+    searchInput = new Fl_Input(10, 35, 139, 30);
+    searchInput->align(Fl_Align(FL_ALIGN_CENTER));
+                                              
+    searchButton = new Fl_Button(159, 35, 72, 30, "search"); 
+    searchButton->callback(searchButtonCallback, this);
+
+
     Fl_Group *o = new Fl_Group(241, 35, 374, 410);
     o->box(FL_DOWN_BOX);
-    { new Fl_Output(302, 45, 303, 30, "Name"); } // Fl_Output* o
-    {
-      Fl_Button *o = new Fl_Button(533, 85, 72, 30, "Play");
-      o->deactivate();
-    } // Fl_Button* o
-    {
-      Fl_Table *o = new Fl_Table(251, 150, 354, 285, "DLC");
-      o->end();
-    } // Fl_Table* o
-    {
-      Fl_Button *o = new Fl_Button(451, 85, 72, 30, "Install");
-      o->deactivate();
-    } // Fl_Button* o
-    {
-      Fl_Button *o = new Fl_Button(369, 85, 72, 30, "Uninstall");
-      o->deactivate();
-    } // Fl_Button* o
-    o->end();
-  } // Fl_Group* o
+    gameName = new Fl_Output(302, 45, 303, 30, "Name"); 
+    
+    playButton = new Fl_Button(533, 85, 72, 30, "Play");
+    playButton->deactivate();
+    playButton->callback(playButtonCallback, this);
 
+    innstallButton = new Fl_Button(451, 85, 72, 30, "Install");
+    innstallButton->callback(instalButtonCallback, this);
+
+    innstallButton->deactivate();
+      
+
+    uninnstallButton = new Fl_Button(369, 85, 72, 30, "Uninstall");
+    uninnstallButton->callback(uninstalButtonCallback, this);
+    uninnstallButton->deactivate();
+     
+    
+    dlcTab = new Fl_Table(251, 150, 354, 285, "DLC");
+    o->end();
   end();
 }
