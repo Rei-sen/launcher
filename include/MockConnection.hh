7f47775f548b168946150fc2c14978df997f2e29
rei-sen <Qasde@Protonmail.ch>
2022-01-04 18:41:44 +0100

    inicjalizacja bazy danych

--- include/MockConnection.hh
+++ include/MockConnection.hh
@@ -9,14 +9,18 @@ public:
   MockConnection &operator=(const MockConnection &) = delete;
 
   ~MockConnection() = default;
-  MockConnection() = default;
+  MockConnection();
 
   bool isConnected() override;
   bool login(std::string name, std::string pass) override;
   bool registerAccount(std::string name, std::string pass) override;
 
-  bool updateGame(std::string title,std::string description,int price); //dodaje też grę jeśli nie ma jej na liście
-  bool updateNews(std::string gametitle, std::string title, std::string content); //dodaje też grę jeśli nie ma jej na liście
-  bool updateSocials(std::string medium, std::string link); //dodaje też grę jeśli nie ma jej na liście
-
+  bool updateGame(std::string title, std::string description,
+                  int price); // dodaje też grę jeśli nie ma jej na liście
+  bool
+  updateNews(std::string gametitle, std::string title,
+             std::string content); // dodaje też grę jeśli nie ma jej na liście
+  bool
+  updateSocials(std::string medium,
+                std::string link); // dodaje też grę jeśli nie ma jej na liście
 };
--- src/MockConnection.cc
+++ src/MockConnection.cc
@@ -1,15 +1,36 @@
 
 #include "MockConnection.hh"
 
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <sstream>
+
+#include <sqlite3.h>
+
+// zamienić potem na unique_ptr
+static sqlite3 *createLocalDatabase();
+
+MockConnection::MockConnection() {
+  // sprawdzić czy istnieje
+  // if (exists) {
+  //
+  //}
+  // else {
+  auto db = createLocalDatabase();
+  sqlite3_close(db); // usunąć potem
+  //}
+}
+
 bool MockConnection::isConnected() { return true; }
 bool MockConnection::login(std::string name, std::string pass) {
 
-//#warning zaimplementować
+  //#warning zaimplementować
   return true;
 }
 bool MockConnection::registerAccount(std::string name, std::string pass) {
 
-//#warning zaimplementować
+  //#warning zaimplementować
   return false;
 }
 
@@ -27,4 +48,25 @@ bool MockConnection::updateNews(std::string gametitle, std::string title,
 bool MockConnection::updateSocials(std::string medium, std::string link) {
   //#warning zaimplementować
   return false;
+}
+
+static sqlite3 *createLocalDatabase() {
+  sqlite3 *db;
+  int result = sqlite3_open("server/db.sqlt", &db);
+
+  if (result != SQLITE_OK) {
+    std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+    sqlite3_close(db);
+
+    // throw
+    return nullptr;
+  }
+
+  std::ifstream f("server/init.sql");
+  std::stringstream buffer;
+  buffer << f.rdbuf();
+
+  sqlite3_exec(db, buffer.str().c_str(), nullptr, nullptr, nullptr);
+
+  return db;
 }
