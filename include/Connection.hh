421585e01997a3de25e4dad8c0325fb60d6c9304
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-06 14:21:49 +0100

    dodanie metod wirtualnych

--- include/Connection.hh
+++ include/Connection.hh
@@ -1,7 +1,8 @@
-﻿
+
 #pragma once
 
 #include <string>
+#include <vector>
 
 class Connection {
 public:
@@ -14,6 +15,7 @@ public:
   virtual bool isConnected() = 0;
   virtual bool login(std::string name, std::string pass) = 0;
   virtual bool registerAccount(std::string name, std::string pass) = 0;
-
-
+  virtual std::vector<std::string> getAllGames()=0;
+  virtual std::vector<std::string> getAllDlc() = 0;
+  virtual bool updatePassword(std::string pass) =0;
 };
