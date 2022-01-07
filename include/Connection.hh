50f012da6f236c3f5a4f507d948a226c1a2fc704
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-08 00:35:25 +0100

    Dodanie nowych metod wirtualnych

--- include/Connection.hh
+++ include/Connection.hh
@@ -1,8 +1,12 @@
 
 #pragma once
 
+#include <optional>
 #include <string>
 #include <vector>
+#include "GameInfo.hh"
+#include "DLCInfo.hh"
+
 
 class Connection {
 public:
@@ -14,8 +18,21 @@ public:
   virtual ~Connection() = default;
   virtual bool isConnected() = 0;
   virtual bool login(std::string name, std::string pass) = 0;
-  virtual bool registerAccount(std::string name, std::string pass) = 0;
-  virtual std::vector<std::string> getAllGames()=0;
-  virtual std::vector<std::string> getAllDlc() = 0;
-  virtual bool updatePassword(std::string pass) =0;
+
+  virtual std::optional<std::string> registerAccount(std::string name,
+                                                     std::string pass) = 0;
+  virtual bool
+  updateLoginInfo(
+      std::string name, std::string pass) = 0; // przetestować jeszcze trzeba
+
+  virtual std::vector<GameInfo> getAllGames() = 0;
+  virtual std::vector<DLCInfo> getAllDlc(int64_t id) = 0;
+
+  virtual std::vector<GameInfo> getOwnedGames(int64_t userID) = 0;
+  virtual std::vector<DLCInfo> getOwnedDlc(int64_t userID, int64_t gameID) = 0;
+  virtual DLCInfo getDLCInfo(int64_t DlcID) = 0;
+  virtual GameInfo getGameInfo(int64_t gameID) = 0;
+
+
+
 };
