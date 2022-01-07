cd9a23e91b271250bdd8bdb63236f48fccb0ed9c
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-08 00:36:20 +0100

    Dodanie definicji nagłówków metod

--- include/MockConnection.hh
+++ include/MockConnection.hh
@@ -2,10 +2,14 @@
 #pragma once
 
 #include <memory>
+
+#include <sqlite3.h>
 #include <vector>
 
 #include "Connection.hh"
-#include <sqlite3.h>
+#include "UserInfo.hh"
+
+
 
 class MockConnection : public Connection {
 public:
@@ -17,7 +21,8 @@ public:
 
   bool isConnected() override;
   bool login(std::string name, std::string pass) override;
-  bool registerAccount(std::string name, std::string pass) override;
+  std::optional<std::string> registerAccount(std::string name,
+                                             std::string pass) override;
 
   bool updateGame(std::string title, std::string description,
                   int price); // dodaje też grę jeśli nie ma jej na liście
@@ -28,94 +33,26 @@ public:
   updateSocials(std::string medium,
                 std::string link); // dodaje też grę jeśli nie ma jej na liście
 
-  std::vector<std::string> getAllGames() override;
-
-  std::vector<std::string> getAllDlc() override;
-
-  bool updatePassword(std::string pass) override; // przetestować jeszcze trzeba
-
   struct DBDeleter {
     void operator()(sqlite3 *ptr) { sqlite3_close(ptr); }
   };
 
   using db_ptr = std::unique_ptr<sqlite3, DBDeleter>;
 
-  int getUserId();
+    bool
+  updateLoginInfo(std::string name,
+                  std::string pass)
+      override; // przetestować jeszcze trzeba
+  std::vector<GameInfo> getAllGames() override;
+
+  std::vector<DLCInfo> getAllDlc(int64_t id) override;
+
+  std::vector<GameInfo> getOwnedGames(int64_t userID) override;
+  std::vector<DLCInfo> getOwnedDlc(int64_t userID, int64_t gameID) override;
+  DLCInfo getDLCInfo(int64_t DlcID) override;
+  GameInfo getGameInfo(int64_t gameID) override;
 
 private:
   db_ptr db = nullptr;
-  int userId;
+  std::optional<UserInfo::ID> userID;
 };
-
-/*
-gotowe zapytania sql do których można robić metody
-zapytania sql
-////////////////////////////////
-logowanie
-select users.id
-from users
-where users.name = 'user1' and users.password = '123'
-//////////////////////////////////////////////////
-rejestracja
-poprzedozne zapytanie login i sprawdzeniem czy jest jakiś wynik
-insert into users (name, password, publisher)
-values ('aaa', '123', 0)
-////////////////////////////////
-dodawanie gry
-insert into games (name, description, price)
-values ('warhammer', 'fajna gra rts', 21)
-//////////////////////////////////
-wybranie wszystkich gier
-select *
-from games
-///////////////////////////////////
-info o dlc
-select *
-from dlcs
-where gameID = '2'
-////////////////////////////////////
-utworzenie newsa
-problem
-////////////////////////////////////
-info o grze
-select *
-from games
-where games.id = 1
-/////////////////////
-dostań posiadane gry
-Select gameOwnership.gameID
-from gameOwnership
-where gameOwnership.userID = 3
-////////////////////////
-is logged in - no nie wiem jak to zrobić
-///////////////////////////////////////
-posiadane dlc
-select *
-from dlcOwnership
-where dlcOwnership.userID = 3
-////////////////////
-update info o grze
-UPDATE games
-SET description = 'Alfred Schmidt'
-WHERE games.id = 3;
-/////////////////////////////////////
-update login
-UPDATE users
-SET name = 'Alfred'
-WHERE users.id = 3;
-////////////////////////////
-update password
-UPDATE users
-SET password = '123'
-WHERE users.id = 3
-/////////////////////////
-update news
-UPDATE news
-SET content = 'asdwqe asd awd waeqe asdaw'
-WHERE news.id = 1
-*/
-
-
-
-
-
