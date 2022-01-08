ad40f2299699d21d4389e1a48bdcc48ad4783cdb
rei-sen <Qasde@Protonmail.ch>
2022-01-08 16:43:45 +0100

    reszta stanu i getUserInfo

--- include/Connection.hh
+++ include/Connection.hh
@@ -1,8 +1,11 @@
 ﻿
 #pragma once
 
+#include <memory>
 #include <optional>
 #include <string>
+
+#include "UserInfo.hh"
 
 class Connection {
 public:
@@ -14,7 +17,10 @@ public:
   virtual ~Connection() = default;
   virtual bool isConnected() = 0;
   virtual bool login(std::string name, std::string pass) = 0;
+  virtual bool isLoggedIn() = 0;
 
   virtual std::optional<std::string> registerAccount(std::string name,
                                                      std::string pass) = 0;
+
+  virtual std::unique_ptr<UserInfo> getUserInfo() = 0;
 };
--- include/State.hh
+++ include/State.hh
@@ -1,17 +1,33 @@
 
 #pragma once
 
+#include <map>
 #include <memory>
 
 #include <FL/Fl_Group.H>
 
 #include "Connection.hh"
+#include "DLCInfo.hh"
+#include "GameInfo.hh"
+#include "UserInfo.hh"
 
 class State {
 public:
+  State(const State &) = delete;
+  State &operator=(const State &) = delete;
+
+  ~State() = default;
+  State() = delete;
+  State(std::unique_ptr<Connection> &&connection);
+
   Connection &getConnection();
-  void setConnection(std::unique_ptr<Connection> &&Connection);
+
+  void update();
 
 private:
-  std::unique_ptr<Connection> con;
+  std::unique_ptr<Connection> connection;
+  std::unique_ptr<UserInfo> user;
+
+  std::vector<GameInfo> allGames;
+  std::map<GameInfo::ID, std::map<DLCInfo::ID, DLCInfo>> dlcs;
 };
