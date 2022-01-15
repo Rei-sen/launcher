0df66b79817a9f57bbfcdd8b6a4deee0f491121a
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-15 17:08:26 +0100

    zmiana nazwy wektora + puste wyszukanie = pełna lista

--- src/UI/GameLibTab.cc
+++ src/UI/GameLibTab.cc
@@ -9,6 +9,8 @@
 #include <FL/Fl_Table.H>
 #include <FL/fl_ask.H>
 
+
+
 void GameLibTab::onPlayButton(Fl_Widget *, void *v) {
   fl_message("Game launched");
 }
@@ -25,6 +27,7 @@ void GameLibTab::onSearchButton(Fl_Widget *, void *v) {
   ((GameLibTab *)v)->gameName->value(((GameLibTab *)v)->searchInput->value());
   std::string temp = ((GameLibTab *)v)->searchInput->value();
   ((GameLibTab *)v)->userSearch(temp);
+
 }
 
 void GameLibTab::onBrowserClick(Fl_Widget *, void *v) {
@@ -34,15 +37,15 @@ void GameLibTab::onBrowserClick(Fl_Widget *, void *v) {
 
 void GameLibTab::onDLCBrowserClick(Fl_Widget *, void *v) {
 
-  // tego ostatecznie ma nie być prawda?
+// tego ostatecznie ma nie być prawda?
+
 }
 
-// może troszkę myloncje jest tutaj to gameID ale teraz niewiem jak to nazwać
-// innaczej
+// może troszkę myloncje jest tutaj to gameID ale teraz niewiem jak to nazwać innaczej
 void GameLibTab::updateGameDLCsList(int gameID) {
   dlcList->clear();
   ownedGameDLCsVect.clear();
-
+  
   GameInfo tempGame = state.getConnection().getAllGames()[gameID - 1];
   std::vector<std::pair<GameInfo::ID, DLCInfo::ID>> tempOwnedDlCs =
       state.getConnection().getOwnedDLCs();
@@ -50,13 +53,13 @@ void GameLibTab::updateGameDLCsList(int gameID) {
   std::vector<DLCInfo> tempDLCs;
   for (auto ownedProd : tempOwnedDlCs) {
     if (ownedProd.first == gameID) {
-      tempDLCs.push_back(tempGame.getDLCs()[ownedProd.second - 1]);
-      dlcList->add(
-          tempGame.getDLCs()[ownedProd.second - 1].getTitle().c_str());
-      ownedGameDLCsVect.push_back(tempGame.getDLCs()[ownedProd.second - 1]);
+      tempDLCs.push_back(tempGame.getDLCs()[ownedProd.second-1]);
+      dlcList->add(tempGame.getDLCs()[ownedProd.second-1].getTitle().c_str());
+      ownedGameDLCsVect.push_back(tempGame.getDLCs()[ownedProd.second-1]);
     }
   }
 }
+
 
 void GameLibTab::userSearch(std::string value) {
   int i = 0;
@@ -84,7 +87,7 @@ void GameLibTab::userSearch(std::string value) {
         dlcList->clear_active();
         gameList->clear();
         ownedGameDLCsVect.clear();
-        ownedGamesVect.clear();
+        shownGames.clear();
       }
       gameList->add(game.getTitle().c_str(), (void *)this);
       tempVect.push_back(game);
@@ -92,10 +95,13 @@ void GameLibTab::userSearch(std::string value) {
     }
   }
   if (tempVect.size()) {
-    ownedGamesVect.clear();
-    ownedGamesVect = tempVect;
+    shownGames.clear();
+    shownGames = tempVect;
   } else {
     fl_alert("No game found");
+    for (auto ownedGame:tempOwnedGames) {
+        gameList->add(ownedGame.getTitle().c_str());
+    }
   }
 }
 
@@ -107,14 +113,14 @@ void GameLibTab::initGamesList() {
     for (auto owned : ownedID) {
       if (owned == game.getID()) {
         gameList->add(game.getTitle().c_str(), (void *)this);
-        ownedGamesVect.push_back(game);
+        shownGames.push_back(game);
       }
     }
   }
   updateGamesList();
 }
 
-void GameLibTab::updateGamesList() {
+void GameLibTab::updateGamesList() { 
   int i = gameList->value();
   if (i == 0) {
     gameName->clear_active();
@@ -127,7 +133,7 @@ void GameLibTab::updateGamesList() {
 
   } else {
     --i;
-    auto selectedGame = ownedGamesVect[i];
+    auto selectedGame = shownGames[i];
     gameName->set_active();
     gameName->value(selectedGame.getTitle().c_str());
     updateGameDLCsList(selectedGame.getID());
@@ -175,4 +181,5 @@ GameLibTab::GameLibTab(State &s) : Tab("Game library", s) {
   end();
 
   initGamesList();
+
 }
