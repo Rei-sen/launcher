ecab469ac30c105bde0bb4ce1c7280efe5bdd652
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-18 17:26:16 +0100

    Backend zakładki sklepu

--- include/UI/NewsTab.hh
+++ include/UI/NewsTab.hh
@@ -3,11 +3,33 @@
 
 #include "UI/Tab.hh"
 
+#include <FL/Fl_Text_Display.H>
+#include <FL/Fl_Tree.H>
+#include <GameInfo.hh>
+#include <News.hh>
+
 class NewsTab : public Tab {
 public:
   NewsTab(const NewsTab &) = delete;
   NewsTab &operator=(const NewsTab &) = delete;
   virtual ~NewsTab() = default;
 
+  static void onNewsTreeSelected(Fl_Widget *, void *);
+
   NewsTab(State &s);
+
+private:
+  std::vector<News> newsVector;
+
+  //  std::vector<GameInfo> gamesWithNewsVector;
+  //
+  void initTabData();
+
+  void updateTabData();
+
+  Fl_Tree *newsTree;
+  Fl_Text_Display *titleField;
+  Fl_Text_Display *contentField;
+  Fl_Text_Buffer *newsTitleBuf;
+  Fl_Text_Buffer *newsContentBuf;
 };
