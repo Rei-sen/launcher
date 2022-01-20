4bc1b877f70134b488c045fdf4af0bf16eaff10d
Adamcwikla99 <56162396+Adamcwikla99@users.noreply.github.com>
2022-01-20 22:27:08 +0100

    Poprawienie metody dodającej wszystkie nowości
    W bonusie usunięcie niepotrzebnych komentarzy

--- src/UI/NewsTab.cc
+++ src/UI/NewsTab.cc
@@ -15,56 +15,18 @@ void NewsTab::initTabData() {
     gameNews = temp[curentNews.getGameID()-1].getTitle() + "/";
     gameNews += curentNews.getTitle();
     newsTree->add(gameNews.c_str());
-
-    /*
-    if (gamesWithNewsVector.size()) {
-      bool alreadyAdded = false;
-      for (auto gameToAdd : gamesWithNewsVector) {
-        if (gameToAdd.getID() == curentNews.getID()) {
-          alreadyAdded = true;
-          break;
-        }
-      }
-      if (!alreadyAdded) {
-        gamesWithNewsVector.push_back(temp[curentNews.getGameID() - 1]);
-      }
-    } else {
-      gamesWithNewsVector.push_back(temp[curentNews.getGameID()-1]);
-    }
-    */
-
-    // to poniżej do testów jest 
-    /*
-    gameNews += "a";
-    newsTree->add(gameNews.c_str());
-    std::string test = "asdwad/asd";
-    newsTree->add(test.c_str());
-    gameNews += "b";
-        newsTree->add(gameNews.c_str());
-    */
-
   }
 
   updateTabData();
 }
 
 void NewsTab::updateTabData() {
-//	int i = newsTree->da
   std::string tempLabel;
     std::vector<GameInfo> temp = state.getConnection().getAllGames();
-  /*
-  for (Fl_Tree_Item *item = newsTree->first(); item;
-       item = newsTree->next(item)) {
-    printf("Item: %s\n", item->label());
-  }
-    */
 
   Fl_Tree_Item *i = newsTree->first_selected_item();
   if (i != nullptr) {
-//    printf("wybrany ");
     tempLabel = i->label();
-//    printf(i->label());
-//   printf("\n");
     for (auto newsElement : newsVector) {
         if (newsElement.getTitle() == tempLabel) {
           titleField->set_active();
@@ -79,12 +41,7 @@ void NewsTab::updateTabData() {
     titleField->clear_active();
     contentField->clear_active();
   }
-
-
-
-
-
-
+  newsTree->root_label("News list");
 }
 
 
@@ -106,10 +63,6 @@ NewsTab::NewsTab(State &s) : Tab("News", s) {
   contentField = new Fl_Text_Display(241, 75, 374, 370);  // Fl_Text_Display* o
   contentField->buffer(newsContentBuf);
 
-//  newsVector = state.getConnection().getAllNews();
-  
-//  printf(newsVector[0].getTitle().c_str());
-//
   initTabData();
   end();
 }
